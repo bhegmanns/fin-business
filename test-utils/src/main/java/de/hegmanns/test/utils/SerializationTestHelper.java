142f60e1a61ae31bb669751271f8361e9b1debdf
Bernd Hegmanns <it@hegmanns.de>
2017-10-10 21:40:14 +0200

    add javadoc for SerializationTestHelper

--- test-utils/src/main/java/de/hegmanns/test/utils/SerializationTestHelper.java
+++ test-utils/src/main/java/de/hegmanns/test/utils/SerializationTestHelper.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InvalidClassException;
 import java.io.NotSerializableException;
 import java.io.ObjectOutputStream;
+import java.io.Serializable;
 
 /**
  * Simple helper class to serialize an object into a stream and returns if serialization succeeded.
@@ -15,11 +16,34 @@ import java.io.ObjectOutputStream;
 public class SerializationTestHelper {
 	
 	/**
+	 * Result for serialization.
 	 * 
 	 * @author B. Hegmanns
 	 */
-	public static enum SerialationTestResult{OK, ERROR_NOT_SERIALIZABLE, ERROR_IO, ERROR_INVALID_CLASS, ERROR_TECHNICAL};
+	public static enum SerialationTestResult{
+		/** successful serialization */
+		OK, 
+		
+		/** object isn't serializable (interface isn't implemented) */
+		ERROR_NOT_SERIALIZABLE, 
+		
+		/** IO error, object cannot write for example*/
+		ERROR_IO, 
+		
+		/** all error from {@link InvalidClassException} */
+		ERROR_INVALID_CLASS, 
+		
+		/** other technical error */
+		ERROR_TECHNICAL};
 
+	/**
+	 * Serializes an instance to the currently open {@link ObjectOutputStream}.
+	 * Note, that the instance/class have to implement {@link Serializable}-interface.
+	 * 
+	 * @param instance the (serializable) instance
+	 * @param outputStream the output stream where the object would be written
+	 * @return {@link SerialationTestResult#OK} is it works other see {@link SerialationTestResult}
+	 */
 	public static SerialationTestResult serializeIntoStream(Object instance, ObjectOutputStream outputStream){
 		SerialationTestResult result = SerialationTestResult.OK;
 		try {
@@ -38,6 +62,12 @@ public class SerializationTestHelper {
 		return result;
 	}
 	
+	/**
+	 * Checks if the instance should be serialized.
+	 * 
+	 * @param instance the instance
+	 * @return {@link SerialationTestResult#OK} if works, other see {#link {@link SerialationTestResult}
+	 */
 	public static SerialationTestResult checkSerialize(Object instance){
 		try(ObjectOutputStream outputStream = new ObjectOutputStream(new ByteArrayOutputStream())){
 			return serializeIntoStream(instance, outputStream);
